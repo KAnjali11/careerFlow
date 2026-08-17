82c4a39024d7fc3a527033c155bead7ad40cb873
KAnjali11 <anjalikurva05@gmail.com>
2026-08-17 22:24:07 +0530

    Demonstrate singleton bean state sharing

--- src/main/java/org/jobTracking/careerflow/CareerFlowApplication.java
+++ src/main/java/org/jobTracking/careerflow/CareerFlowApplication.java
@@ -1,6 +1,6 @@
 package org.jobTracking.careerflow;
 import org.jobTracking.careerflow.service.JobApplicationService;
-
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +10,16 @@ public class CareerFlowApplication {
     public static void main(String[] args) {
 
         var context=SpringApplication.run(CareerFlowApplication.class, args);
-        JobApplicationService service1=context.getBean(JobApplicationService.class);
-        JobApplicationService service2=context.getBean(JobApplicationService.class);
-        System.out.println(service1 == service2);
+//        JobApplicationService service1=context.getBean(JobApplicationService.class);
+//        JobApplicationService service2=context.getBean(JobApplicationService.class);
+//        System.out.println(service1 == service2);
+        JobApplicationService user1=context.getBean(JobApplicationService.class);
+        user1.setLastSearchedCompany("Amazon");
+        JobApplicationService user2=context.getBean(JobApplicationService.class);
+        user2.setLastSearchedCompany("Google");
+        System.out.println("User 1 searched :" + user1.getLastSearchedCompany());
+        System.out.println("User 2 searched : " + user2.getLastSearchedCompany());
+        System.out.println("User 1 Now sees:" + user1.getLastSearchedCompany());
     }
 
 }
--- src/main/java/org/jobTracking/careerflow/service/JobApplicationService.java
+++ src/main/java/org/jobTracking/careerflow/service/JobApplicationService.java
@@ -3,9 +3,18 @@ import  org.springframework.stereotype.Service;
 import org.jobTracking.careerflow.repository.JobApplicationRepository;
 @Service
 public class JobApplicationService {
+
+    private String lastSearchedCompany;
     private final JobApplicationRepository jobApplicationRepository;
     public JobApplicationService (JobApplicationRepository jobApplicationRepository){
         this.jobApplicationRepository=jobApplicationRepository;
 
+    }
+
+    public void setLastSearchedCompany(String lastSearchedCompany) {
+        this.lastSearchedCompany = lastSearchedCompany;
+    }
+    public String getLastSearchedCompany(){
+        return lastSearchedCompany;
     }
 }
