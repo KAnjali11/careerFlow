2eb4e6ef9e77d2e478d424829ac767716870647b
KAnjali11 <anjalikurva05@gmail.com>
2026-08-25 00:13:20 +0530

    Added REST controller endpoint

--- src/main/java/org/jobTracking/careerflow/controller/JobApplicationController.java
+++ src/main/java/org/jobTracking/careerflow/controller/JobApplicationController.java
@@ -1,11 +1,20 @@
 package org.jobTracking.careerflow.controller;
 import org.springframework.stereotype.Component;
 import  org.jobTracking.careerflow.service.JobApplicationService;
-@Component
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/applications")
 public class JobApplicationController {
     private final JobApplicationService jobApplicationService;
     public JobApplicationController(JobApplicationService jobApplicationService){
         this.jobApplicationService=jobApplicationService;
+    }
+    @GetMapping
+    public String test(){
+        return "Job Application Controller is working";
     }
 
 }
